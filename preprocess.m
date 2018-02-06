9dd7128c311a52a436da072615767035b6e1aebe
pangsm0415 <36044760+pangsm0415@users.noreply.github.com>
2018-02-06 17:08:07 +0000

    Update preprocess.m

--- preprocess.m
+++ preprocess.m
@@ -5,4 +5,5 @@ switch datatype
         output = yael_vecs_normalize(input,2,0);
     case {'siaMac','netvlad'}
         output = sign(input) .* abs(input) .^ 0.5;
+        output = yael_vecs_normalize(input,2,0);
 end
